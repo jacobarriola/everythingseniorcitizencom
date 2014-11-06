d0a5966e2d134e29a045245fbd023b3cd8792e15
Jacob Arriola <jacob.arriola@gmail.com>
2014-11-06 15:19:52 -0800

    created new home page

---
+++ front-page.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<p>This is the front.php page</p>
+<p>This is the front.php page</p>
+<p>This is the front.php page</p>
+<p>This is the front.php page</p>
+<p>This is the front.php page</p>
+<p>This is the front.php page</p>
+<?php get_footer(); ?>

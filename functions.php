c5b352a46b195c1647602bac9f8dc243ef620855
Jacob Arriola <jacob.arriola@gmail.com>
2014-11-06 15:04:46 -0800

    initialize child theme

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+// Load parent style sheet
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
+function enqueue_parent_theme_style() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
+
+
+?>

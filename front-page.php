f5026ced0d3f43e5a76816d0903e1443e8c1df2e
Jacob Arriola <jacob.arriola@gmail.com>
2014-11-06 18:45:54 -0800

    more changes

--- front-page.php
+++ front-page.php
@@ -1,8 +1,14 @@
 <?php get_header(); ?>
-<p>This is the front.php page</p>
-<p>This is the front.php page</p>
-<p>This is the front.php page</p>
-<p>This is the front.php page</p>
-<p>This is the front.php page</p>
-<p>This is the front.php page</p>
+<div class="hero">
+	<div class="hero-inner">
+    <a href="" class="hero-logo"><img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_logo_1.png
+" alt="Logo Image"></a>
+		<div class="hero-copy">
+			<h1>Everything Senior Citizen</h1>
+			<p>A few reasons why this product is worth using, who it's for and why they need it.</p>	
+		</div>
+    <a href="<?php get_bloginfo('stylesheet_directory')?>/listing"><button>View Listings &#8594;</button></a>
+	</div>
+</div>
+
 <?php get_footer(); ?>

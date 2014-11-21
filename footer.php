b488287445d19cbeb8bbf63d194a3bbc25d67844
jacobarriola <jacob.arriola@gmail.com>
2014-11-20 21:19:57 -0800

    testing

--- footer.php
+++ footer.php
@@ -6,7 +6,6 @@
 		<div class="footer-copyright">
 			<p>&copy; <?php echo date( "Y" ) ?> Everything Senior Citizen.  All Rights Reserved.</p>
 		</div>
-
 		</div> <!-- end .container -->
 	</footer> <!-- end #main-footer -->
 <?php endif; ?>

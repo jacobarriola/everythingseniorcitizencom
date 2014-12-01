fa9c557032ae771e392c5104bd5bc7e868d060a8
jacobarriola <jacob.arriola@gmail.com>
2014-11-30 21:34:10 -0800

    gulp init

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp 			= require('gulp'),
+	watch			= require('gulp-watch'),
+	sass			= require('gulp-ruby-sass'),
+	autoprefixer	= require('gulp-autoprefixer'),
+	minifycss		= require('gulp-minify-css'),
+	browserSync		= require('browser-sync'),
+	filter			= require('gulp-filter')
+	notify			= require('gulp-notify');
+
+gulp.task('browser-sync', function() {
+	var files = [
+		'**/*.php'
+	];
+
+	browserSync.init(files, {
+		proxy: 'everythingseniorcitizen:8888/'
+	});
+});
+
+gulp.task('sass', function() {
+		gulp.src('sass/**/*.scss')
+		.pipe(sass({
+			style: 'expanded',
+			lineNumbers: true
+		}))
+		.pipe(filter('**/*.css')) // ensures only *.css files reach .reload
+		.pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+		// .pipe(gulp.dest('.'))
+		// .pipe(rename({suffix: '.min'}))		
+		// .pipe(minifycss({
+		// 	keepSpecialComments:0
+		// }))
+		.pipe(gulp.dest('.'))
+		.pipe(browserSync.reload({stream:true}))
+		.pipe(notify({
+			message: "You just Sassified your files!"
+		}));
+
+});
+
+
+gulp.task('default', ['sass', 'browser-sync'], function () {
+	gulp.watch('sass/**/*.scss', ['sass']);
+});
